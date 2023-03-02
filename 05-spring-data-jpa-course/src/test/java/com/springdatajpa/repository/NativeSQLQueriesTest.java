4f803327c869aea01e3e0e78c36745a5af18fe19
Eliel Herrera Gress <elieta103@gmail.com>
2023-03-02 14:38:54 -0600

    Pagination and Sorting

---
+++ 05-spring-data-jpa-course/src/test/java/com/springdatajpa/repository/NativeSQLQueriesTest.java
@@ -0,0 +1,35 @@
+package com.springdatajpa.repository;
+
+import com.springdatajpa.entity.Product;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class NativeSQLQueriesTest {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Test
+    void findByNameOrDescriptionSQLIndexParamMethod(){
+
+        Product product = productRepository.findByNameOrDescriptionSQLIndexParam(
+                "Product 10", "Product 10 Description"
+        );
+
+        System.out.println("---------------------- Inicio Resultados --------------------------------");
+        System.out.println(product.toString());
+        System.out.println("---------------------- Final Resultados --------------------------------");
+    }
+
+    @Test
+    void findByNameOrDescriptionSQLNamedParamMethod(){
+        Product product = productRepository.findByNameOrDescriptionSQLNamedParam("Product 10"
+                        , "Product 10 Description");
+
+        System.out.println("---------------------- Inicio Resultados --------------------------------");
+        System.out.println(product.toString());
+        System.out.println("---------------------- Final Resultados --------------------------------");
+    }
+}
---
+++ 05-spring-data-jpa-course/src/test/java/com/springdatajpa/repository/PaginationAndSortingTest.java
@@ -0,0 +1,123 @@
+package com.springdatajpa.repository;
+
+import com.springdatajpa.entity.Product;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+
+import java.util.List;
+
+
+@SpringBootTest
+public class PaginationAndSortingTest {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Test
+    void paginationTest(){
+        int pageNo = 3;
+        int pageSize = 5;
+
+        //Create pageable object
+        Pageable pageable = PageRequest.of(pageNo, pageSize);
+
+        //FindAll method and pass pageable instance
+        Page<Product> page = productRepository.findAll(pageable);
+
+        System.out.println("---------------------- Inicio Resultados --------------------------------");
+        List<Product> products = page.getContent();
+        products.forEach(System.out::println);
+
+        System.out.println("Total de paginas : "+ page.getTotalPages());
+        System.out.println("Total de elementos : "+ page.getTotalElements());
+        System.out.println("Número de elementos : "+ page.getNumberOfElements());
+        System.out.println("Tamaño : "+ page.getSize());
+        System.out.println("Es ultimo ? : "+ page.isLast());
+        System.out.println("Es primero ? : "+ page.isFirst());
+
+        System.out.println("---------------------- Final Resultados --------------------------------");
+    }
+
+    @Test
+    void sortingTest(){
+        String sortBy = "price";
+        String sortDir = "desc";
+
+        Sort sort = sortDir.equalsIgnoreCase(Sort.Direction.ASC.name())? Sort.by(sortBy).ascending() :
+                                                                         Sort.by(sortBy).descending();
+        List<Product> productList = productRepository.findAll(sort);
+
+        //List<Product> productList = productRepository.findAll(Sort.by(sortBy).ascending());
+
+
+        System.out.println("---------------------- Inicio Resultados --------------------------------");
+        productList.forEach(System.out::println);
+
+        System.out.println("---------------------- Final Resultados --------------------------------");
+
+    }
+
+
+    @Test
+    void sortingMultipleFieldTest(){
+        String strName = "name";
+        String strDesc = "description";
+        String strDir = "desc";
+
+
+        Sort sortByName = strDir.equalsIgnoreCase(Sort.Direction.ASC.name())? Sort.by(strName).ascending() :
+                                                                              Sort.by(strName).descending();
+        Sort sortByDesc = strDir.equalsIgnoreCase(Sort.Direction.ASC.name())? Sort.by(strDesc).ascending() :
+                                                                              Sort.by(strDesc).descending();
+
+        Sort groupBySort = sortByName.and(sortByDesc);
+
+        List<Product> productList = productRepository.findAll(groupBySort);
+
+        System.out.println("---------------------- Inicio Resultados --------------------------------");
+        productList.forEach(System.out::println);
+        System.out.println("---------------------- Final Resultados --------------------------------");
+
+    }
+
+    @Test
+    void paginationAndSortingTogetherTest(){
+        String strByPrice = "price";
+        String strDir = "desc";
+        int pageNo = 0;
+        int pageSize = 5;
+
+        //Sort Object
+        Sort sort = strDir.equalsIgnoreCase(Sort.Direction.ASC.name()) ? Sort.by(strByPrice).ascending() :
+                                                                         Sort.by(strByPrice).descending();
+
+        //Pageable Object
+        Pageable pageable = PageRequest.of(pageNo, pageSize, sort);
+
+        Page<Product> page = productRepository.findAll(pageable);
+
+        System.out.println("---------------------- Inicio Resultados --------------------------------");
+        List<Product> products = page.getContent();
+        products.forEach(System.out::println);
+
+        System.out.println("Total de paginas : "+ page.getTotalPages());
+        System.out.println("Total de elementos : "+ page.getTotalElements());
+        System.out.println("Número de elementos : "+ page.getNumberOfElements());
+        System.out.println("Tamaño : "+ page.getSize());
+        System.out.println("Es ultimo ? : "+ page.isLast());
+        System.out.println("Es primero ? : "+ page.isFirst());
+
+        System.out.println("---------------------- Final Resultados --------------------------------");
+
+    }
+}
+
+
+
+
+
