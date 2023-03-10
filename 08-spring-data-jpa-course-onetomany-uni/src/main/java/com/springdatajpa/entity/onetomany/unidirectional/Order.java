a599e5dae076a4a5d399ad1fc3d8867ca0d51393
Eliel Herrera Gress <elieta103@gmail.com>
2023-03-09 21:17:31 -0600

    OneToMany Unidirectional y Bidirectional Order OrderItems

---
+++ 08-spring-data-jpa-course-onetomany-uni/src/main/java/com/springdatajpa/entity/onetomany/unidirectional/Order.java
@@ -0,0 +1,53 @@
+package com.springdatajpa.entity.onetomany.unidirectional;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "orders")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String orderTrackingNumber;
+    private int totalQuantity;
+    private BigDecimal totalPrice;
+    private String status;
+
+    @CreationTimestamp
+    private LocalDateTime dateCreated;
+
+    @UpdateTimestamp
+    private LocalDateTime lastUpdated;
+
+    @OneToOne(cascade = CascadeType.ALL, mappedBy = "order")
+    private Address billingAddress;
+
+    //Default fetch type for one to many is LAZY
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinColumn(name = "order_id", referencedColumnName = "id")
+    private Set<OrderItem> orderItems = new HashSet<>();
+
+
+    public BigDecimal getTotalAmount(){
+        BigDecimal amount = new BigDecimal(0.0);
+        for(OrderItem item : this.orderItems){
+            amount = amount.add(item.getPrice());
+        }
+        return amount;
+    }
+}
